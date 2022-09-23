8f72d23923eaafe396a38aa3e05d1b6fc24a2021
Carlos Del Castillo M <maussac13@gmail.com>
2022-09-23 17:00:26 -0500

    primercommit con angular

---
+++ src/app/recap.ts
@@ -0,0 +1,18 @@
+const username: string = 'Carlos';
+
+const sum = (a: number, b: number) => {
+  return a + b;
+
+}
+sum(4,6);
+
+class Person {
+  age: number;
+  lastName: string;
+
+  constructor(age: number, lastName: string){
+    this.age = age;
+    this.lastName = lastName;
+  }
+
+}
